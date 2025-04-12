24a68dc4f3fab946c914bf2ed820d4969a83fe66
JekaCAP <agentgeka@yandex.ru>
2025-04-12 14:11:26 +0500

    subHabit

---
+++ src/main/java/ru/jeka/habit/tracker/model/SubHabit.java
@@ -0,0 +1,29 @@
+package ru.jeka.habit.tracker.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class SubHabit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private int completedCount;
+
+    @ManyToOne
+    @JoinColumn(name = "habit_id") // Связь с родительской привычкой
+    @JsonBackReference
+    private Habit habit;
+
+    public void incrementCompleted() {
+        completedCount++;
+    }
+}
